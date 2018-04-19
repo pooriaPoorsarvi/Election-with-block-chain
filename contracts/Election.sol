6f8393688d5fa131fc3a0af90eab9553b75a5307
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2018-04-20 01:09:17 +0430

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.2;
+/**
+ * The Election contract does this and that...
+ */
+contract Election {
+	string public candidate;
+	function Election () public {
+		candidate = "Candidate 1";
+	}	
+}
