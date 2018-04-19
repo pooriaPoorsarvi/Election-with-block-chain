7de52741535a1f22c2375b4140aae91e9f7b5129
pooriaPoorsarvi <pooriapoorsarvi@gmail.com>
2018-04-20 02:09:56 +0430

    second commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,8 +3,33 @@ pragma solidity ^0.4.2;
  * The Election contract does this and that...
  */
 contract Election {
-	string public candidate;
+	
+
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+		
+	}
+	
+
+	mapping (uint => Candidate) public candidates;
+	
+	uint public candidatesCount;
+
+
 	function Election () public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
 	}	
+
+
+	function addCandidate (string _name) private {
+		candidatesCount ++ ;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+		
+	}
+	
+
+
 }
